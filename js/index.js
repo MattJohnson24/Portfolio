b03adaa90f39cdec873de3a62feafe9c34271d8c
MattJohnson24 <matt.r.johnson24@gmail.com>
2024-04-15 22:36:39 -0400

    Responsiveness

---
+++ js/index.js
@@ -0,0 +1,21 @@
+function toggleMenu(){
+    if(document.getElementById('drop-down-menu').style.display === "flex"){
+        document.getElementById('drop-down-menu').style.display = "none";
+        document.getElementsByClassName('menu-icon')[0].style.display = "none";
+    }
+    else{
+        document.getElementById('drop-down-menu').style.display = "flex";
+        document.getElementById('menu-icon').style.display = "none";
+    }
+
+    // if(document.getElementById('drop-down-menu').style.filter === "invert(86%) sepia(53%) saturate(1708%) hue-rotate(290deg) brightness(90%) contrast(81%)"){
+    //     document.getElementById('drop-down-menu').style.filter = "";
+    // }
+    // else{
+    //     document.getElementById('drop-down-menu').style.display = "invert(86%) sepia(53%) saturate(1708%) hue-rotate(290deg) brightness(90%) contrast(81%)";
+    // }
+}
+
+function closeMenu(){
+    document.getElementById('drop-down-menu').style.display === "none"
+}
